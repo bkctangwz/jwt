d7c65414b1c1bb206305a7eb0eb122adc3b75122
Wenzhong Tang <Wenzhong.Tang@bakerhughes.com>
2023-01-28 21:36:27 -0600

    end of section 2

---
+++ NZWalks/NZwalks.API/Models/Domain/Walk.cs
@@ -0,0 +1,16 @@
+﻿namespace NZwalks.API.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public  double length { get; set; }
+        public Guid RegionId { get; set; }
+        public Guid WalkDifficultyId { get; set; }
+
+        //navigation properties
+        public Region Region { get; set; }
+        public WalkDifficulty WalkDifficulty { get; set; }
+
+    }
+}
